21845d59c8bf54cf817cc17d082adc7f0786eb21
HalReal <85908977+HalReal@users.noreply.github.com>
2021-07-01 15:26:47 -0600

    Add files via upload

---
+++ ProductosIniciales.java
@@ -0,0 +1,40 @@
+package productos;
+
+public class ProductosIniciales {
+
+		String [] Productos = new String [30];{
+				Productos [0] = " Jabón ";
+				Productos [1] = " Papel de baño ";
+				Productos [2] = " Chicles ";
+				Productos [3] = " Galletas ";
+				Productos [4] = " Café ";
+				Productos [5] = " Gaseosas ";
+				Productos [6] = " Agua ";
+				Productos [7] = " Helados ";
+				Productos [8] = " Cuadernos ";
+				Productos [9] = " Lápices ";
+				Productos [10] = " Vasos Plásticos ";
+				Productos [11] = " Manzanas ";
+				Productos [12] = " Cerezas ";
+				Productos [13] = " Refrescos ";
+				Productos [14] = " Leche ";
+				Productos [15] = " ";
+				Productos [16] = "";
+				Productos [17] = "";
+				Productos [18] = "";
+				Productos [19] = "";
+				Productos [20] = "";
+				Productos [21] = "";
+				Productos [22] = "";
+				Productos [23] = "";
+				Productos [24] = "";
+				Productos [25] = "";
+				Productos [26] = "";
+				Productos [27] = "";
+				Productos [28] = "";
+				Productos [29] = "";
+				Productos [30] = "";
+				
+		}
+	
+}
