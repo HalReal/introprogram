b10876f9d27a99204c8a9a7e12036198d508833e
HalReal <85908977+HalReal@users.noreply.github.com>
2021-07-01 21:57:04 -0600

    Add files via upload

---
+++ Examen1.java
@@ -0,0 +1,15 @@
+package paquete;
+
+import java.util.Scanner;
+import java.util.Calendar;
+import java.util.InputMismatchException;
+
+public class Examen {
+	static Scanner lector = new Scanner(System.in);
+	static Scanner lector2 = new Scanner(System.in);
+	static boolean condicional = true;
+	
+	public void pedirDatos(String nombre[], double precio[], double costo[], int num1[], int num2[]) {
+		
+}
+
